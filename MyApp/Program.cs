f7d81cb32d01e531e99e97b4e67ede949089ea95
MarianoGomesOpitz <marianogomesopitz@gmail.com>
2022-08-26 18:41:56 -0300

    del 1 al 3

---
+++ MyApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+//punto 1
+    try
+    {
+        Console.WriteLine("Ingrese un numero, no sea boludo.");
+        int texto =  Convert.ToInt32(Console.ReadLine());
+        
+    }
+    catch (System.Exception)
+    {
+        Console.WriteLine("Sos un boludo.");
+        
+    }
+
+
+//punto2
+
+try
+{
+    Console.WriteLine("Ingrese un numero.");
+    float dividendo =  Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Ingrese otro numero.");
+    float divisor =  Convert.ToInt32(Console.ReadLine());
+
+    float total = dividendo/divisor;
+
+    Console.WriteLine("Total "+total );
+}
+catch (FormatException ex)
+{
+    Console.WriteLine("ingreso valor invlido");
+    
+}
+catch (Exception ex){
+     Console.WriteLine("error");
+}
+
+//punto 3
+
+try
+{
+    Console.WriteLine("Ingrese kilometros conducidos.");
+    float kilometros =  Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Ingrese litros usados.");
+    float litros =  Convert.ToInt32(Console.ReadLine());
+
+    
+
+    float total = kilometros/litros;
+
+    Console.WriteLine("Total "+total );
+}
+catch (Exception ex)
+{
+    Console.WriteLine("error en los datos");
+    
+}
